b0e670db6910eea5a4fc4f6cf72f4c07a43cf850
root <nnastaran83@gmail.com>
2023-09-27 09:52:57 +0000

    q9 done

---
+++ algorithms/q9-mergeSort.js
@@ -0,0 +1,42 @@
+/**
+ * mergeSort
+ * @param {number[]} array
+ * @returns {number} Sorted array
+ */
+function mergeSort(array) {
+  if (array.length === 1 || array.length === 0) {
+    return array;
+  }
+
+  const splitIndex = Math.floor(array.length / 2);
+  return merge(
+    mergeSort(array.slice(0, splitIndex)),
+    mergeSort(array.slice(splitIndex))
+  );
+}
+
+/**
+ * merge
+ * @param {number[]} arr1 Sorted array
+ * @param {number[]} arr2 Sorted array
+ * @returns {number[]} Sorted merge of arr1 and arr2
+ */
+function merge(arr1, arr2) {
+  let merged = [];
+  while (arr1.length !== 0 && arr2.length !== 0) {
+    if (arr1[0] < arr2[0]) {
+      merged.push(arr1.shift());
+    } else if (arr2[0] < arr1[0]) {
+      merged.push(arr2.shift());
+    } else {
+      merged.push(arr1.shift(), arr2.shift());
+    }
+  }
+  return [...merged, ...arr1, ...arr2];
+}
+
+console.log(
+  mergeSort([
+    1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92,
+  ])
+);
