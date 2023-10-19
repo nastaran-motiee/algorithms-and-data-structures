eb2079b439443d1b9a370148424d7d9a35c3c889
Nastaran Motiee <33676320+nastaran-motiee@users.noreply.github.com>
2023-10-19 17:41:52 +0000

    q11

---
+++ algorithms/q11-rotateArray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  let last = k % nums.length;
+
+  if (nums.length > 1 && last !== 0) {
+    let start = nums.length - last;
+    nums.splice(
+      last,
+      nums.length,
+      ...nums.splice(0, start, ...nums.slice(-last))
+    );
+  }
+
+  return nums;
+};
