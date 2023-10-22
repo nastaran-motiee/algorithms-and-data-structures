eb9d088306efce236b4a0bda1a89bbf3ac95c6bb
Nastaran Motiee <33676320+nastaran-motiee@users.noreply.github.com>
2023-10-22 11:42:58 +0000

    added union to set

--- data-structures/q4-set.js
+++ data-structures/q4-set.js
@@ -1,37 +1,57 @@
 /**
  * @see {@link https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/create-a-set-class}
+ *  @see {@link https://www.freecodecamp.org/learn/coding-interview-prep/data-structures/perform-a-union-on-two-sets}
  */
 class Set {
   constructor() {
-    // Dictionary will hold the items of our set
+    // This will hold the set
     this.dictionary = {};
     this.length = 0;
   }
-
   // This method will check for the presence of an element and return true or false
   has(element) {
     return this.dictionary[element] !== undefined;
   }
-
   // This method will return all the values in the set
   values() {
     return Object.values(this.dictionary);
   }
-
-  add(val) {
-    if (!Object.hasOwn(this.dictionary, val)) {
-      this.dictionary[val] = val;
+  // This method will add an element to the set
+  add(element) {
+    if (!this.has(element)) {
+      this.dictionary[element] = element;
+      this.length++;
       return true;
     }
+
     return false;
   }
+  // This method will remove an element from a set
+  remove(element) {
+    if (this.has(element)) {
+      delete this.dictionary[element];
+      this.length--;
+      return true;
+    }
 
-  remove(val) {
-    return delete this.dictionary[val];
+    return false;
+  }
+  // This method will return the size o;f the set
+  size() {
+    return this.length;
   }
 
-  size() {
-    return Object.keys(this.dictionary).length;
+  union(setB) {
+    const thisDictValues = this.values();
+    const setBValues = setB.values();
+    const unionSet = new Set();
+
+    for (let i = 0; i < thisDictValues.length; i++) {
+      unionSet.add(thisDictValues[i]);
+    }
+    for (let i = 0; i < setBValues.length; i++) {
+      unionSet.add(setBValues[i]);
+    }
+    return unionSet;
   }
 }
-
